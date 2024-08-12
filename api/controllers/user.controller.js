b5e06691e1069859ca9b09b99627116d9565b979
Zoran Nikolov <zoranstrumica7@gmail.com>
2024-08-12 08:14:53 +0200

    complete update user profile page functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -3,7 +3,7 @@ import { errorHandler } from "../utils/error.js";
 import User from "../models/user.model.js";
 
 export const test = (req, res) => {
-  res.json({ message: "Api is working" });
+  res.json({ message: "API is working!" });
 };
 
 export const updateUser = async (req, res, next) => {
