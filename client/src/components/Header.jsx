58606e5b0a324b6653a3c46b0751d0de503a93a2
Zoran Nikolov <zoranstrumica7@gmail.com>
2024-08-12 10:45:43 +0200

    add signout gunctionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -11,12 +11,30 @@ import { IoIosSearch } from "react-icons/io";
 import { FaMoon, FaSun } from "react-icons/fa"; //dodadeno FaSun
 import { useSelector, useDispatch } from "react-redux"; //dodadeno useDispatch
 import { toggleTheme } from "../redux/theme/themeSlice"; //dodadeno
+import { signoutSuccess } from "../redux/user/userSlice";
 
 export default function Header() {
   const path = useLocation().pathname;
   const dispatch = useDispatch(); //dodadeno
   const { currentUser } = useSelector((state) => state.user);
   const { theme } = useSelector((state) => state.theme); //dodadeno
+
+  const handleSignout = async () => {
+    try {
+      const res = await fetch("/api/user/signout", {
+        method: "POST",
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        console.log(data.message);
+      } else {
+        dispatch(signoutSuccess());
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   return (
     <Navbar className="border-b-2">
       <Link
@@ -65,7 +83,7 @@ export default function Header() {
             <Link to={"/dashboard?tab=profile"}>
               <DropdownItem>Profile</DropdownItem>
               <Dropdown.Divider />
-              <Dropdown.Item>Sign out</Dropdown.Item>
+              <Dropdown.Item onClick={handleSignout}>Sign out</Dropdown.Item>
             </Link>
           </Dropdown>
         ) : (
