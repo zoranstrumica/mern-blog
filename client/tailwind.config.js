73e5d9bb1911350aeb721daf1d8a2a53f82e2198
Zoran Nikolov <zoranstrumica7@gmail.com>
2024-08-17 07:32:57 +0200

    add Comments section to the admin dashboard

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,12 +1,17 @@
+const flowbite = require("flowbite-react/tailwind");
+
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
+    flowbite.content(), // Додај го Flowbite React content path
   ],
   theme: {
     extend: {},
   },
-  plugins: [require("flowbite/plugin"), require("tailwind-scrollbar")],
+  plugins: [
+    flowbite.plugin(), // Додај го Flowbite React plugin
+    require("tailwind-scrollbar"),
+  ],
 };
